3d941c5b458623e385ce4c33854ef622d8651ec5
Divyaaa552 <228r1a0552@cmrec.ac.in>
2024-03-11 22:06:33 +0530

    pythonProject1.iml

---
+++ datatypes.py
@@ -0,0 +1,16 @@
+myset=set()
+myset.add(10)
+myset.add(15)
+myset.add(20)
+myset.add(25)
+print(myset)
+s={1,2,3,4,5,6,7,8}
+print(s)
+s.add(10)
+s.add(11)
+s.add(1)
+s1={10,10,20,20}
+print(s1)
+d1={1:"one",2:"two",3:"three",4:"four",5:"five",6:"six"}
+print(d1.keys())
+print(d1.values())
